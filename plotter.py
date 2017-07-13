ef03a7fbfe43867433297e4e54ab249419980a1f
Ali Hasan <aliimad.hasan@mail.utoronto.ca>
2017-07-13 15:46:06 +0000

    Initial python commit

---
+++ plotter.py
@@ -0,0 +1,20 @@
+# Plotting of fst climatologies previously made by monthly_mean
+##### IMPORTS #####
+import numpy as np
+import rpnpy.librmn.all as rmn
+import rpnpy.vgd.all as vgd
+import time, glob, math
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from levels import *
+
+##### FXNS #####
+build_zonal(fst_file):
+    '''
+    builds a zonal mean of the species based on an open fst file.
+    '''
+    pass
+
+
+##### MAIN #####
+
