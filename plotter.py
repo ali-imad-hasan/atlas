d81a9a91fa01560c525a6778779e757a653ac82d
Ali Hasan <aliimad.hasan@mail.utoronto.ca>
2017-07-25 18:06:23 +0000

    Added colorbar functionality

--- plotter.py
+++ plotter.py
@@ -7,10 +7,11 @@ import time, glob, math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from levels import *
 
 ##### CONSTANTS #####
-fsts = glob.glob("/space/hall1/sitestore/eccc/aq/r1/alh002/FST_STORAGE/*.fst"); fsts.sort()
+fsts = glob.glob("/space/hall1/sitestore/eccc/aq/r1/alh002/FST_STORAGE/BAK2/output_*.fst"); fsts.sort()
 ppb_conv = 2.897e10
 resolution = 1.125
 lev, ni, nj = 59, 320, 161
@@ -46,29 +47,35 @@ zonals = params
 for fst in fsts:
     # ZONAL MEAN CALCULATION #
     # extraction of grid data from fst files
-    file_id = rmn.fstopenall(fst)
-    for param in params.keys():
-        if param in special_params.keys():
-            var_name = special_params[param]
-        else:
-            var_name = param.upper()
+    print fsts
+    try:
+        file_id = rmn.fstopenall(fst)
+        for param in params.keys():
+            if param in special_params.keys():
+                var_name = special_params[param]
+            else:
+                var_name = param.upper()
 
-        grid_data = np.zeros((lev, ni, nj))
-        v_grid = vgd.vgd_read(file_id)  # gets the vgd object
-        ip1s = vgd.vgd_get(v_grid, 'VIPM')
-        # builds pressure levels
-        pres_levs = [rmn.convertIp(rmn.CONVIP_DECODE, ip1, rmn.KIND_PRESSURE)[0] for ip1 in ip1s]
-        
-        for k, ip in enumerate(ip1s):
-            rec_key = rmn.fstinf(file_id, ip1=ip, nomvar=var_name)  # returns key based on params
-            grid_data[k] = rmn.fstluk(rec_key)['d']  # reads grid data
+            grid_data = np.zeros((lev, ni, nj))
+            v_grid = vgd.vgd_read(file_id)  # gets the vgd object
+            ip1s = vgd.vgd_get(v_grid, 'VIPM')
+            # builds pressure levels
+            pres_levs = [rmn.convertIp(rmn.CONVIP_DECODE, ip1, rmn.KIND_PRESSURE)[0] for ip1 in ip1s]
+            
+            for k, ip in enumerate(ip1s):
+                rec_key = rmn.fstinf(file_id, ip1=ip, nomvar=var_name)  # returns key based on params
+                grid_data[k] = rmn.fstluk(rec_key)['d']  # reads grid data
 
-        params[param] = grid_data * (ppb_conv / plprms[param]['mw'])
-    latkey = rmn.fstinf(file_id, nomvar='^^')
-    lonkey = rmn.fstinf(file_id, nomvar='>>')
-    lats = rmn.fstluk(latkey)['d'][0]
-    lons = rmn.fstluk(lonkey)['d']
-    rmn.fstcloseall(file_id)
+            params[param] = grid_data * (ppb_conv / plprms[param]['mw'])
+        latkey = rmn.fstinf(file_id, nomvar='^^')
+        lonkey = rmn.fstinf(file_id, nomvar='>>')
+        lats = rmn.fstluk(latkey)['d'][0]
+        lons = rmn.fstluk(lonkey)['d']
+        rmn.fstcloseall(file_id)
+
+    except:
+        print "Error opening/operating on file"
+        exit()
 
     ni, nj, nk = len(lons), len(lats), len(pres_levs)
     # builds zonal mean
@@ -82,7 +89,7 @@ for fst in fsts:
     x_axis = np.zeros((nk,nj))
     y_axis = np.zeros((nk,nj))
     for k in xrange(nk):
-        x_axis[k] = lats
+        x_axis[k] = lats[::-1]
     for j in xrange(nj):
         y_axis[:,j] = pres_levs
     for param in params.keys():
@@ -102,6 +109,13 @@ for fst in fsts:
         # the 20 doesnt matter, it can be any gun as it's a zonal mean
         varplt1 = ax1.contourf(x_axis, y_axis, zonals[param][:,20,:],
                                cmap=c_map, norm=norm, levels=c_values, fontsize=11)
+
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes("right", size="5%", pad=0.1)
+
+        plt.colorbar(varplt1, cax=cax)
+
+        # colorbar
         plt.show()
     exit()
 
